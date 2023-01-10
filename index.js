37304e556773bca1338033d3a615ae5a4e7adc39
khalid <khalid.a.montes@gmail.com>
2023-01-10 17:49:00 -0500

    commit tree 2886c2478bdd3ee07bd443b886d26d3b799b8b62

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const main = document.getElementsByTagName("main")[0];
+main.remove();
+const newHeader = document.createElement("H1");
+    newHeader.setAttribute(`id`, `victory`);
+    newHeader.innerHTML = `khalid is the champion`;
+append.newHeader
+const h1 = document.createElement("h1");
+newHeader.append(h1)
